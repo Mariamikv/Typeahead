3a88dd03543aebee3d743c8ff07bd310c474893d
mariamikv <mariamikv71@gmail.com>
2022-06-27 22:07:29 +0400

    update

--- src/components/List.js
+++ src/components/List.js
@@ -5,16 +5,13 @@ import "../assets/styles/style.css"
 
 function List({users}) {
   return (
-    <div>
-      <ul className="list-container">
+    <ul className="list-container">
       {
         users.map((user, index) => (
           <Card user={user} key={index}/>
         ))
       }
     </ul>
-    </div>
-    
   )
 }
 
